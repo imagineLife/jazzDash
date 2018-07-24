3b880730cb4400c6db8c3931fb816c8a1d2e9b91
Eric Laursen <ericjlaursen@gmail.com>
2018-07-23 21:36:51 -0400

    initial.

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/api/*', (req,res) => {
+	res.json({ok: true})
+})
+
+app.listen(PORT, () => {
+	console.log(`server running on ${PORT}`)
+})
+
+module.exports = {app}

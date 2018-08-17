ca1b6daee65ce5cddefc1f789d61a2087ff76828
Eric Laursen <ericjlaursen@gmail.com>
2018-08-17 05:30:50 -0400

    Includes findById

--- server/routes/musicians/router.js
+++ server/routes/musicians/router.js
@@ -2,6 +2,19 @@ const express = require('express');
 const router = express.Router();
 const { Musician } = require('./models');
 
+//Get by ID
+router.get('/:id', (req, res) => {
+    Musician
+    .findById(req.params.id)
+    .exec()
+    .then(MusicianRes => res.status(201).json(MusicianRes))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({error: 'something went terribly wrong'});
+    });
+  }
+);
+
 //Get Musician with populated musicians ID array
 router.get('/', (req, res) => {
     Musician

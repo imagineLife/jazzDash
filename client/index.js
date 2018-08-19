6f5f83f448dc49d9cbac09ec217c020ef404572e
Eric Laursen <ericjlaursen@gmail.com>
2018-08-19 12:19:55 -0400

    Imports & uses redux store.

--- client/index.js
+++ client/index.js
@@ -1,6 +1,12 @@
 import App from "./App";
-import style from "./main.css";
+import "./main.css";
 import ReactDOM from "react-dom";
 import React from "react";
+import { Provider } from 'react-redux';
+import { devstore } from './store';
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+	<Provider store={devstore}>
+		<App />
+	</Provider>, 
+	document.getElementById("app"));

ed5e30cacf01e5bd18aef1f1f2dee90150cb37cc
Eric Laursen <ericjlaursen@gmail.com>
2018-08-19 11:09:11 -0400

    Includes constructor.

--- client/containers/JazzDash.js
+++ client/containers/JazzDash.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 
 class JazzDash extends Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			dashData : []
+		}
+	}
 
 	render(){
 		return (

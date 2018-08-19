e488200c20628ca4297d2ed3ace8527c32e0f5d7
Eric Laursen <ericjlaursen@gmail.com>
2018-08-18 20:24:35 -0400

    Includes demo JazzDash Container

---
+++ client/containers/JazzDash.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class JazzDash extends Component {
+
+	render(){
+		return (
+		    <React.Fragment>
+		      <p> JazzDash Container Here :) </p>
+		    </React.Fragment>
+		);
+	}
+
+};
+export default JazzDash;

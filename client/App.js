b3f36924d3863229d7a20fa96c694d0ced4657b1
Eric Laursen <ericjlaursen@gmail.com>
2018-08-18 20:19:16 -0400

    converts to class, includes router.

--- client/App.js
+++ client/App.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-const App = () => {
-  return (
-    <React.Fragment>
-      <p>Dummy React Component here!</p>
-    </React.Fragment>
-  );
+import {
+    BrowserRouter as Router,
+    Route,
+    Redirect,
+    Switch
+} from 'react-router-dom';
+
+class App extends Component {
+
+	render(){
+		return (
+		    <React.Fragment>
+		      <p>Dummy React Component here!</p>
+		    </React.Fragment>
+		);
+	}
+
 };
 export default App;

8174bb7b378020a23a556664285b143c9ca7551c
Eric Laursen <ericjlaursen@gmail.com>
2018-08-19 06:42:18 -0400

    renames file.

---
+++ client/components/Nav/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './index.css';
+import DashIcon from '../../imgs/dashboard.png';
+import AboutIcon from '../../imgs/about.png';
+import NavLink from '../NavLink';
+
+export default function Nav(){
+//MAYBE update this specific navLink text to change when signed-in-or-not :) 
+
+//these are properties of each NavLink below	
+	const navLinkArray = [
+		{
+			linkTo : "/",
+			imgSrc : DashIcon,
+			alt : "Dashboard Homepage"
+		},
+		{
+			linkTo : "/about",
+			imgSrc : AboutIcon,
+			alt : "About"
+		}
+	];
+
+//convert the array objects above into <NavLink />s
+	const linkObjsToComponents = navLinkArray.map((navLink,ind) => {
+		return <NavLink 
+			key={ind} 
+			linkTo={navLink.linkTo}
+			imgSrc={navLink.imgSrc}
+			linkTitle={navLink.alt}
+		/>;
+	})
+	
+    return (
+		<nav>
+			<ul>
+				{linkObjsToComponents}
+			</ul>
+		</nav> 
+    );
+}

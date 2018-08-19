5a775fdbdea4ea0b0639c574bcad876d4c52e8e8
Eric Laursen <ericjlaursen@gmail.com>
2018-08-19 19:10:46 -0400

    Includes cors.

--- server/server.js
+++ server/server.js
@@ -15,6 +15,19 @@ mongoose.Promise = global.Promise;
 app.use(bodyParser.json());
 app.use(express.static(__dirname +'/public'));
 
+
+// CORS
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
+
 //ROUTES
 app.use('/api/musicians', musiciansRouter);
 app.use('/api/songs', songsRouter);

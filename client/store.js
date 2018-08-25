6a9af791334d6605e90a0773bbc186c4ac4081da
Eric Laursen <ericjlaursen@gmail.com>
2018-08-25 17:31:51 -0400

    Reduces store content, breaks into other files.

--- client/store.js
+++ client/store.js
@@ -1,59 +1,45 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
-import {createLogger} from 'redux-logger';
+import { fromJS } from 'immutable';
+import { createLogger } from 'redux-logger';
+import createReducer from './reducers';
+import thunk from 'redux-thunk';
 
 //1. initialState
-const initialState = {
-	dashData : []
-}
+const initialState = { dashData : [] }
 
 //2. logger middle-ware
 const loggerMW = createLogger()
 
-//3. dummy reducer to start
-const dashDataReducer = (state=initialState.dashData, action) => {
-	console.log('inside dashDataReducer')
-	switch(action.type){
-		case ("fetch_dashboard_data"):
-			console.log('fetching data on CWM')
-		    fetch(`http://localhost:8080/api/rawData/default`, {
-		        method: 'GET',
-		        headers: {
-		            'Content-Type': 'application/json',
-		        }
-		    })
-		    .then(res => res.json())
-		    .then(res => {
-		    	console.log('fetch res')
-		    	console.log(res)
-		    	console.log('fetch -----')
-		    })
-		    .catch(err => {
-		        const {code} = err;
-		        if (code === 401) {
-		          console.log(code);
-		        }
-		    })
-			// state = Object.assign({}, state, {age: action.payload});
-	}
-	return state;
-}
-
-//4. reducing reducers... not sure I need this for now?
-const reducedReducers = combineReducers({
-	dashData: dashDataReducer,
-})
-
-//5. applying middleware
+//3. applying middleware
 const appliedLoggerMW = applyMiddleware(loggerMW);
 
-//6. CREATE the store
-const store = createStore(reducedReducers, appliedLoggerMW)
 
-//SUBSCRIBE to the store
-//may not need this with the redux logger & the redux dev tools
+//4. export default
+export default function myStoreConfig(initialState){
 
-store.subscribe(() => {
-	console.log('store changed ->', store.getState())
-}) 
+	//5. enhancers
+	const enhancers = [ applyMiddleware() ];
 
-module.exports = { store };
+	//6. conditionally use the enhancers?
+	const composeEnhancers =
+			//validate NOT in production
+		    process.env.NODE_ENV !== 'production' &&
+		    //validate NOT testing, testing doesn't use a window
+		    typeof window === 'object' &&
+		    /*
+		    	IF those are true, does this xtnxn xst?
+			*/
+		    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+		      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
+
+
+	//7. put it all together with createStore
+	const store = createStore(createReducer(), fromJS(initialState), composeEnhancers(...enhancers));
+	return store;
+
+
+	//8. added the myConfigStore fn to index.js...
+	//9. added initial store to index.js
+}
+
+// module.exports = { store };

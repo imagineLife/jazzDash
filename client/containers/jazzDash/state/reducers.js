fadb634dc698429a619d930ed46b03ed8f23b1fe
Eric Laursen <ericjlaursen@gmail.com>
2018-08-28 11:22:44 -0400

    Begins parsing data from mongo to chart-specific objects.

--- client/containers/jazzDash/state/reducers.js
+++ client/containers/jazzDash/state/reducers.js
@@ -1,13 +1,61 @@
 import * as jazzDashConst from './constants';
 
+/*
+	Writing in this manner allows reacts 'this'
+writing function(data) would make 'this' reference this function
+fat-arrow makes 'this' reference the place the fn was called from
+	
+	object for more random data-using, as oppoesd to array for linear  looping 
+
+	key  = chart1
+
+*/
+
+const parseData = function(data){
+	let readyData = data.map((d, i) => {
+		console.log('parseData MAPPING data i')
+		console.log(i)
+		console.log('d')
+		console.log(d)
+		console.log('- - - -')
+		return { 
+			musician: d._musician,
+			song: d._song,
+			noteTypesByPercentage: {
+				CTs: d.CTs,
+				NCTs: d.NCTs,
+				DNCTs: d.totalDiatonicNCTs,
+				NDNCTs: d.totalNonDiatonicNCTs
+			}
+		};
+	})
+
+	console.log('readyData')
+	console.log(readyData)
+	console.log('- - - -')
+
+	return readyData;
+
+}
+
 const dashDataReducer = (state={}, action) => {
 	console.log('inside dashDataReducer')
 	
 	switch(action.type){
 		case jazzDashConst.FETCH_DATA :
-			console.log('fetching dashboard data by reducer...')
-			return state = { ...state, jazzData: action.payload }
-			//return alleviates break
+			// console.log('fetching dashboard data by reducer...')
+			
+			//call the fn
+			const parsedData = parseData(action.payload)
+
+
+/*
+	static data in one-object
+	editing data in unique object
+		like updating socket weather app or something
+*/
+			return state = { ...state, jazzData: parsedData, unparsed: action.payload }
+				//return alleviates 'break'
 	}
 	return state;
 }

9d429d143386f903d454e61003e94372324ea784
Eric Laursen <ericjlaursen@gmail.com>
2018-09-01 09:08:44 -0400

    Includes comments for future querying.c

--- server/routes/rawdatas/router.js
+++ server/routes/rawdatas/router.js
@@ -18,6 +18,12 @@ router.get('/mus/:id', (req, res) => {
 
 //find stats by song ID
 router.get('/song/:id', (req, res) => {
+
+	//will need to.. 
+	//get songID.then()
+	//get RawDataIDs where songID = songID.then()
+	//get RawDatas by those IDs.then()
+
 	RawData.find({_song: req.params.id})
 	.populate('noteLengths', 'length count')
 	.populate('chordStats', 'chordName chordTones diatonicNCTs nonDiatonicNCTs totalNotes')
@@ -34,8 +40,6 @@ router.get('/default', (req, res) => {
 	RawData.find({
     	'_id': { $in: ['5b69712ebecd53905d3ee1f5','5b70868f687ffb3971a867fb']}
 	})
-	// RawData.find({_song: "5b59a3a4e8aaa194e036155a"})
-	// RawData.findById("5b69712ebecd53905d3ee1f5")
 	.populate('noteLengths', 'length count')
 	.populate('_musician', 'first last')
 	.populate('chordStats', 'chordName chordTones diatonicNCTs nonDiatonicNCTs totalNotes')

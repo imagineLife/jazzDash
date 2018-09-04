3c3c75b455b154b284de6ad1b592e620fdfe5ff2
Eric Laursen <ericjlaursen@gmail.com>
2018-09-04 19:18:41 -0400

    Adds div class from data, adds dev border around div.

--- client/components/CountByNoteName/index.js
+++ client/components/CountByNoteName/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import ResponsiveWrapper from '../ResponsiveWrapper';
 import './index.css';
 
-export default class CountByNoteName extends React.Component {
+class CountByNoteName extends React.Component {
 
 	render(){
 		console.log('RENDERING!! CountByNoteName props')
+		// console.log(this.props.data)
 		console.log(this.props.data)
-
+		let thisClass = `CountByNoteName gr-${this.props.data[0].grWidth}`
 		return (
-		    <div className="CountByNoteName">
+		    <div className={thisClass}>
 		      <p>CountByNoteName Here!</p>
 			</div>
 		);
 	}
 }
+
+export default ResponsiveWrapper(CountByNoteName);

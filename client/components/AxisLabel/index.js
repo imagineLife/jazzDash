e7080ae0caef5c87cead5c120b093bbac76b9b47
Eric Laursen <ericjlaursen@gmail.com>
2018-09-05 20:05:27 -0400

    Changes fill to black

--- client/components/AxisLabel/index.js
+++ client/components/AxisLabel/index.js
@@ -10,7 +10,7 @@ export default function AxisLabel(props){
         y={props.yPos}
         transform={props.transformation}
         textAnchor='middle'
-        fill={'#EEEEEE'}
+        fill={'black'}
       >{props.textVal}</text>
     </g>
   )

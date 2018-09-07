a1c759b6e7bd8d35f59f4eedc986177f1b21fdbd
Eric Laursen <ericjlaursen@gmail.com>
2018-09-06 21:14:33 -0400

    Makes 6 ticks.

--- client/components/Axis/index.js
+++ client/components/Axis/index.js
@@ -19,7 +19,7 @@ export default class Axis extends Component {
       .scale(this.props.scale)
       .tickSize(-this.props.tickSize)
       .tickPadding([12])
-      .ticks([4])
+      .ticks(6)
 
     d3Select(this.axisElement).call(axis)
   }

bad43ef65b70c80fb18061aaed03a7d2664e564b
Eric Laursen <ericjlaursen@gmail.com>
2018-09-09 09:42:43 -0400

    Placeholder for developing new components

---
+++ client/components/placeholder/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './index.css';
+
+export default function PlaceHolder(props) {
+	console.log('placeholder props')
+	console.log(props)
+	console.log('- - - - -')
+	return(
+		<p>Dummy Recat Component here</p>	
+	);
+}

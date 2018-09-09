6f459f058eeef05fc5194d5ddcb924b7d6a462d6
Eric Laursen <ericjlaursen@gmail.com>
2018-09-09 12:07:47 -0400

    Includes toggle Component.

--- client/components/UpsAndDowns/index.js
+++ client/components/UpsAndDowns/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import './index.css';
+import ResponsiveWrapper from '../ResponsiveWrapper'
+import Toggle from '../Toggle'
 
-export default function UpsAndDowns(props) {
-	console.log('placeholder props')
-	console.log(props)
-	console.log('- - - - -')
-	return(
-		<p>Dummy Recat Component here</p>	
+
+class UpsAndDowns extends React.Component {
+
+	getNamesFromData(data){
+		return {
+			first : data[0].musician, 
+			second : data[1].musician
+		}
+	}
+	render(){
+		return(
+			<React.Fragment>
+				<p>Dummy Recat Component here</p>
+				<Toggle cl='UpsAndDowns' opts={this.getNamesFromData(this.props.data)} onToggle={this.toggle}/>	
+			</React.Fragment>
 	);
+	}
 }
+
+export default ResponsiveWrapper(UpsAndDowns);

0ecbdc1bb087d2ac9d4909f42ae13fb00bfaf8cc
Eric Laursen <ericjlaursen@gmail.com>
2018-09-09 12:01:00 -0400

    placeholder upsAndDowns vis.

---
+++ client/components/UpsAndDowns/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './index.css';
+
+export default function UpsAndDowns(props) {
+	console.log('placeholder props')
+	console.log(props)
+	console.log('- - - - -')
+	return(
+		<p>Dummy Recat Component here</p>	
+	);
+}

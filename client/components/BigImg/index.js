5a639336374a752dbae1f6413d805363f44de534
Eric Laursen <ericjlaursen@gmail.com>
2018-09-09 12:02:12 -0400

    Removes dev comments.

--- client/components/BigImg/index.js
+++ client/components/BigImg/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
-// import {Link} from 'react-router-dom';
 import ResponsiveWrapper from '../ResponsiveWrapper';
 
 import './index.css';
 
 function BigImg(props) {
-	//make class string for svg element
-
-	console.log('BigImg props')
-	console.log(props)
-	console.log('- - - - -')
 	return(
 		<section className='bigImgWrapper'>
 		<img className='bigImg' src={`client/imgs/${props.type}s/${props.str}.jpg`}/>

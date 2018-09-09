4f028d690dd212319828cbe172c1a46a6aac88b8
Eric Laursen <ericjlaursen@gmail.com>
2018-09-09 09:42:59 -0400

    Placeholder for Big Images

---
+++ client/components/BigImg/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './index.css';
+
+export default function BigImg(props) {
+	console.log('BigImg props')
+	console.log(props)
+	console.log('- - - - -')
+	return(
+		<p>Dummy Recat Component here</p>	
+	);
+}

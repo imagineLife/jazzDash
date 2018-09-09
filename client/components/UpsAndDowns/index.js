ed62ed12eff1b8abd24e617d5b6f9191cd3e3a33
Eric Laursen <ericjlaursen@gmail.com>
2018-09-09 12:25:48 -0400

    Cleans up data for d3 consumption, includes contructor etc.

--- client/components/UpsAndDowns/index.js
+++ client/components/UpsAndDowns/index.js
@@ -6,6 +6,16 @@ import Toggle from '../Toggle'
 
 
 class UpsAndDowns extends React.Component {
+	constructor(props){
+		super(props)
+
+		this.state = {
+			margins : { top: 45, right: 40, bottom: 100, left: 70 },
+			curShowing: 0
+		}
+
+		this.convertToArray = this.convertToArray.bind(this)
+	}
 
 	getNamesFromData(data){
 		return {
@@ -13,7 +23,54 @@ class UpsAndDowns extends React.Component {
 			second : data[1].musician
 		}
 	}
+
+	convertToArray(obj, usableKeysArr){
+		let cleanedData = [];
+		usableKeysArr.forEach(key => {
+			let thisObj = {};
+			if(key in obj){
+				thisObj[key] = obj[key];
+				cleanedData.push(thisObj)
+			}
+		})
+		return cleanedData;
+	}
+
+	/*
+	To-Do
+	1. make xAxis
+		hide it, just use domain & range, no actual visible axis component
+	
+	2. Make 2 'rects'...?
+		split width by 3
+		put rects @ thirds
+
+		Make HEIGHT 'length' resemble number of ups / downs traveled
+
+		CURIOUS
+		Make up arrow point stick to Top
+		Make down arrow point stick to Bottom?
+
+		make & append arrowHeads to both,
+			up on top
+			down on bottom
+	
+
+	3.
+
+
+	*/
+
 	render(){
+
+		/*
+			Make data workable with d3 scales
+	    */
+		let curMusicianStats = this.props.data[this.state.curShowing];
+		let dataKeys = ['downs','ups','unis'];
+		let curUsableData = this.convertToArray(curMusicianStats, dataKeys);
+
+
 		return(
 			<React.Fragment>
 				<p>Dummy Recat Component here</p>

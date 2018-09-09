c4b7976079e33ddd7425243bd5d8122f323dd9b9
Eric Laursen <ericjlaursen@gmail.com>
2018-09-09 10:17:20 -0400

    Adds wrapper, img, & h3.

--- client/components/BigImg/index.js
+++ client/components/BigImg/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+// import {Link} from 'react-router-dom';
+import ResponsiveWrapper from '../ResponsiveWrapper';
+
 import './index.css';
 
-export default function BigImg(props) {
+function BigImg(props) {
+	//make class string for svg element
+
 	console.log('BigImg props')
 	console.log(props)
 	console.log('- - - - -')
 	return(
-		<p>Dummy Recat Component here</p>	
+		<section className='bigImgWrapper'>
+		<img className='bigImg' src={`client/imgs/musicians/${props.str}.jpg`}/>
+		<h3 className='bigImgName'>{props.fullName}</h3>
+		</section>
 	);
-}
+};
+
+
+export default ResponsiveWrapper(BigImg);

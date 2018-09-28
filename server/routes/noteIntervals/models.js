dbeeda79bf457503817ac19da8f890b86b55ce29
Eric Laursen <ericjlaursen@gmail.com>
2018-09-28 15:11:24 -0400

    NoteIntervals model update from clone.

--- server/routes/noteIntervals/models.js
+++ server/routes/noteIntervals/models.js
@@ -3,13 +3,14 @@ const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
 
-const NoteLengthSchema = mongoose.Schema({
-	_song: {type: Schema.Types.ObjectId, ref: 'Songs' },
-	_artist : {type: Schema.Types.ObjectId, ref: 'Musicians' },
-	length: Number,
-	count: Number
+const NoteIntervalSchema = mongoose.Schema({
+	count : Number,
+	interval : String,
+	chord: String,
+	song: {type: Schema.Types.ObjectId, ref: 'Songs' },
+	musician : {type: Schema.Types.ObjectId, ref: 'Musicians' },
 })
 
-const noteLength = mongoose.model('NoteLength', NoteLengthSchema);
+const noteInterval = mongoose.model('NoteInterval', NoteIntervalSchema);
 
-module.exports = { noteLength };
+module.exports = { noteInterval };

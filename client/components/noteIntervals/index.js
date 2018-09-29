21d90d225c30e34c4d3cbe532aec32966f847b61
Eric Laursen <ericjlaursen@gmail.com>
2018-09-29 07:31:44 -0400

    Includes d3 methos and calculations.

--- client/components/noteIntervals/index.js
+++ client/components/noteIntervals/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './index.css';
 import ResponsiveWrapper from '../ResponsiveWrapper'
+import * as d3 from 'd3'
 
 class NoteIntervals extends React.Component {
 	constructor(props){
@@ -10,6 +11,25 @@ class NoteIntervals extends React.Component {
 			margins : { top: 20, right: 20, bottom: 100, left: 20 },
 			curShowing: 0
 		}
+
+		this.colorScale = d3.scaleOrdinal(d3.schemeDark2)
+		this.radiusScale = d3.scaleSqrt()
+	}
+
+	makeD3Simulation(){
+		return d3.forceSimulation()
+			.force("yforce", d3.forceY().strength(.03))
+			.force("xforce", d3.forceX().strength(.03))
+	}
+
+	removeLessimportantData(data){
+		let thisArr = []
+		for(let prop in data){
+			if(!['musician','song','grWidth'].includes(prop)){
+				thisArr.push(data[prop])
+			}
+		}
+		return thisArr
 	}
 	
 	render(){
@@ -24,11 +44,22 @@ class NoteIntervals extends React.Component {
 			Make data workable with d3 scales
 	    */
 
-	    //1. Select JUST the selected musician stats
-		let curMusicianStats = this.props.data[this.state.curShowing];
+	    //1. Prep Data for working with d3
+		let curMusicianStats = this.removeLessimportantData(this.props.data[this.state.curShowing]);
+		let countExtent = d3.extent(curMusicianStats, d => d.count)
+		let smallestCircleRad = Math.min(svgDimensions.width, svgDimensions.height)
 
-		console.log('placeholder props')
-		console.log(this.props)
+		this.radiusScale.domain(countExtent).range([0, (smallestCircleRad/ 4)])
+
+		//setup simulation
+		let sim = this.makeD3Simulation();
+		sim.force("myCollide", d3.forceCollide(d => radiusScale(d.count)));
+
+		// console.log('radiusScale.range()')
+		// console.log(this.radiusScale.range())
+
+		console.log('noteIntervals curMusicianStats')
+		console.log(curMusicianStats)
 		console.log('- - - - -')
 		
 		return(

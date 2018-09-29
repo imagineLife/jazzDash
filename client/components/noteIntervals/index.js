84eee12949ed66ee46f5addd72bebd689b374567
Eric Laursen <ericjlaursen@gmail.com>
2018-09-29 06:16:29 -0400

    Makes class and wraps in RespWrapper.

--- client/components/noteIntervals/index.js
+++ client/components/noteIntervals/index.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import './index.css';
+import ResponsiveWrapper from '../ResponsiveWrapper'
 
-export default function PlaceHolder(props) {
-	// console.log('placeholder props')
-	// console.log(props)
-	// console.log('- - - - -')
-	return(
-		<p>Dummy Recat Component here</p>	
-	);
+class NoteIntervals extends React.Component {
+	constructor(props){
+		super(props)
+
+		this.state = {
+			margins : { top: 20, right: 20, bottom: 100, left: 20 },
+			curShowing: 0
+		}
+	}
+	
+	render(){
+
+		//prep svgDimensions var
+		const svgDimensions = {
+	      width: Math.max(this.props.respWrapWidth, 300),
+	      height: 450
+	    }
+
+		/*
+			Make data workable with d3 scales
+	    */
+
+	    //1. Select JUST the selected musician stats
+		let curMusicianStats = this.props.data[this.state.curShowing];
+
+		console.log('placeholder props')
+		console.log(this.props)
+		console.log('- - - - -')
+		
+		return(
+			<p>Dummy React Component here</p>	
+		);
+	}
 }
+
+export default ResponsiveWrapper(NoteIntervals)

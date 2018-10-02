e5c046f97c90c615341b4a6689d6f03fdb787636
Eric Laursen <ericjlaursen@gmail.com>
2018-10-02 17:35:42 -0400

    begins beatCount component

---
+++ client/components/BeatCounts/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './index.css';
+
+export default class BeatCounts extends React.Component {
+	// console.log('placeholder props')
+	// console.log(props)
+	// console.log('- - - - -')
+	render(){
+		return(
+			<p>Dummy Recat Component here</p>	
+		);
+	}
+}

875bcd36f0309dd6721a22ca42c6f7e728e1ffa3
Eric Laursen <ericjlaursen@gmail.com>
2018-10-04 16:44:13 -0400

    Lets BigImg render song title text.

--- client/components/BigImg/index.js
+++ client/components/BigImg/index.js
@@ -4,12 +4,20 @@ import ResponsiveWrapper from '../../containers/ResponsiveWrapper';
 import './index.css';
 
 function BigImg(props) {
-	return(
-		<section className='bigImgWrapper'>
-		<img className='bigImg' src={`client/imgs/${props.type}s/${props.str}.jpg`}/>
-		<h3 className='bigImgName'>{props.fullName}</h3>
-		</section>
-	);
+	if(props.fullName !== 'IF'){
+		return(
+			<section className='bigImgWrapper'>
+			<img className='bigImg' src={`client/imgs/${props.type}s/${props.str}.jpg`}/>
+			<h3 className='bigImgName'>{props.fullName}</h3>
+			</section>
+		);
+	}else{
+		return(
+			<section className='bigImgWrapper'>
+			<h3 className='bigImgName songTitle'>{props.fullName}</h3>
+			</section>
+		);
+	}
 };
 
 

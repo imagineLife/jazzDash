2c09f8f82e5ed2e41dbc2b8a780b9ee32c3a8177
Eric Laursen <ericjlaursen@gmail.com>
2018-10-08 06:50:26 -0400

    Includes marker and path for arrowhead

--- client/components/Def/index.js
+++ client/components/Def/index.js
@@ -5,15 +5,22 @@ import './index.css';
 export default function Def(props){
 
 	return(
-		<defs
-			id={props.id}
-			viewBox={props.viewBox}
-			refX={props.refX}
-			refY={props.refY}
-			markerWidth={props.markerWidth}
-			markerHeight={props.markerHeight}
-			orient={props.orient}
-		/>
+		<defs>
+			<marker
+				id={props.id}
+				viewBox={props.viewBox}
+				refX={props.refX}
+				refY={props.refY}
+				markerWidth={props.markerWidth}
+				markerHeight={props.markerHeight}
+				orient={props.orient}
+			>
+				<path
+					d={props.pathD}
+					className={props.pathCl}
+				/>
+			</marker>
+		</defs>
 	);
 
 }

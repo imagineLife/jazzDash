f2a11c4c598b4c8ad48cddc3dc949b56289fd26a
Eric Laursen <ericjlaursen@gmail.com>
2018-10-08 06:42:26 -0400

    Defs for line-end arrowHeads

---
+++ client/components/Def/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import * as d3 from 'd3';
+import './index.css';
+
+export default function Def(props){
+
+	return(
+		<defs
+			id={props.id}
+			viewBox={props.viewBox}
+			refX={props.refX}
+			refY={props.refY}
+			markerWidth={props.markerWidth}
+			markerHeight={props.markerHeight}
+			orient={props.orient}
+		/>
+	);
+
+}

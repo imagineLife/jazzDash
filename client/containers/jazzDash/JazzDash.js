0afd5416aab7779c75ab2744d00233749480678d
Eric Laursen <ericjlaursen@gmail.com>
2018-10-22 15:43:41 -0400

    Includes DistanceByBeat Component.

--- client/containers/jazzDash/JazzDash.js
+++ client/containers/jazzDash/JazzDash.js
@@ -7,6 +7,7 @@ import NoteLengthCounts from '../noteLengthCounts';
 import NoteTypePercents from '../NoteTypePercents';
 import NoteIntervals from '../NoteIntervals';
 import BeatCounts from '../BeatCounts';
+import DistanceByBeat from '../DistanceByBeat'
 
 import BigImg from '../../components/BigImg';
 import './index.css';
@@ -35,7 +36,7 @@ class JazzDash extends Component {
 		}else{
 
 			console.log('dash wrapper props')
-			console.log(this.props.storeDashData)
+			console.log(this.props)
 			console.log('- - - - -')
 
 			let musicians = this.props.storeDashData.musicians.map(m => {
@@ -62,12 +63,15 @@ class JazzDash extends Component {
 						<UpsAndDowns data={this.props.storeDashData.totalDirections} parentCol={'6'}/>
 					</div>
 					<div className="row">
+						<BeatCounts data={this.props.storeDashData.beatCounts} parentCol={'12'}/>
+					</div>					
+					<div className="row">
 						<NoteTypePercents data={this.props.storeDashData.noteTypesByPercentage} parentCol={'6'}/>
 						<NoteIntervals data={this.props.storeDashData.noteIntervalCounts} parentCol={'6'}/>
 					</div>
 					<div className="row">
-						<BeatCounts data={this.props.storeDashData.beatCounts} parentCol={'12'}/>
-					</div>
+						<DistanceByBeat parentCol={'12'}/>
+					</div>	
 					<div className="bottomSpacer" />
 				</main>
 			)

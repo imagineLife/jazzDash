8cef23aeeb14605546e8fd9682f082c6488ae4ea
Eric Laursen <ericjlaursen@gmail.com>
2018-10-27 19:58:11 -0400

    Removes old code

--- client/components/Bars/index.js
+++ client/components/Bars/index.js
@@ -18,53 +18,32 @@ export default function Bars(props) {
 
     let thisWidth = ( props.barWidth || xScale.bandwidth())
     
-    
-    //ARROWS
-    let firstDataObj = data[0];
-    if ('key' in firstDataObj){
-        bars = (
-          data.map(d => {
+
+    bars = (
+      data.map(d => {
+
+        console.log('map bars d')
+        console.log(d)
+//Count-Per-Note-Name
+        if(d.noteName){
             return ( 
               <Rect
-                key={d.key}
-                x={xScale(d.key) + (xScale.bandwidth() / 2)}
-                y={yScale(d.val)}
-                height={height - margins.bottom - scales.yScale(d.val)}
+                key={d.noteName}
+                x={xScale(d.noteName)}
+                y={yScale(d.count)}
+                height={height - margins.bottom - scales.yScale(d.count)}
                 width={thisWidth}
-                barZeroHeight={height - margins.bottom}
-                fill={'green'}
+                fill={colorScale(d.commodity)}
                 stroke={'green'}
                 strokeWidth={'2px'}
+                // onClick={() => props.showBarDetails(d)}
+                // onMouseOver={() => props.mousedOver(d)}
                 className="singleBar"
               />
             )
-          })
-        )
-    }else{
-        bars = (
-          data.map(d => {
-
-    //Count-Per-Note-Name
-            if(d.noteName){
-                return ( 
-                  <Rect
-                    key={d.noteName}
-                    x={xScale(d.noteName)}
-                    y={yScale(d.count)}
-                    height={height - margins.bottom - scales.yScale(d.count)}
-                    width={thisWidth}
-                    fill={colorScale(d.commodity)}
-                    stroke={'green'}
-                    strokeWidth={'2px'}
-                    // onClick={() => props.showBarDetails(d)}
-                    // onMouseOver={() => props.mousedOver(d)}
-                    className="singleBar"
-                  />
-                )
-            }
-          })
-        )
-    }
+        }
+      })
+    )
 
     return (
         <React.Fragment>

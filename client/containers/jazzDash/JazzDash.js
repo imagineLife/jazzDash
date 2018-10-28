faa03bf26747afda15a34b8282e8bdf79e4d3cb1
Eric Laursen <ericjlaursen@gmail.com>
2018-10-27 20:37:48 -0400

    connects showTooltip to CountByNoteName

--- client/containers/jazzDash/JazzDash.js
+++ client/containers/jazzDash/JazzDash.js
@@ -8,7 +8,7 @@ import NoteTypePercents from '../NoteTypePercents';
 import NoteIntervals from '../NoteIntervals';
 import BeatCounts from '../BeatCounts';
 import DistanceByBeat from '../DistanceByBeat'
-
+import Tooltip from '../../components/Tooltip'
 import BigImg from '../../components/BigImg';
 import './index.css';
 
@@ -16,12 +16,18 @@ class JazzDash extends Component {
 	constructor(props){
 		super(props)
 		this.state = {}
+		this.showTooltip = this.showTooltip.bind(this)
 	}
 
 	componentWillMount(){
 		if(!this.state.dashData){
 			fetchStats(this.props.dispatch)
 		}
+	}
+
+	showTooltip(obj){
+		console.log('PARENT SHOW TOOLTIP!! test')
+		console.log(obj)
 	}
 
 	render(){
@@ -50,13 +56,14 @@ class JazzDash extends Component {
 
 			return(
 				<main className="dashWrapper">
+					<Tooltip />
 					<div className="row">
 						{albumImg}
 						{musicians}
 						{SongName}
 					</div>
 					<div className="row">
-						<CountByNoteName data={this.props.storeDashData.totalsByNoteName} parentCol={'12'}/>
+						<CountByNoteName showTooltip={this.showTooltip} data={this.props.storeDashData.totalsByNoteName} parentCol={'12'}/>
 					</div>
 					<div className="row">
 						<NoteLengthCounts data={this.props.storeDashData.noteLengthCounts} parentCol={'6'}/>

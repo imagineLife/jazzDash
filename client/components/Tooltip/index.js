bf46b3639806d8f58c71011a40d3d6e526d801ab
Eric Laursen <ericjlaursen@gmail.com>
2018-10-28 12:12:46 -0400

    Adds style obj from pros to tooltip style

--- client/components/Tooltip/index.js
+++ client/components/Tooltip/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 import './index.css';
 
 export default function Tooltip(props) {
+	console.log('tooltip props')
+	console.log(props.data)
+	let { pgX, pgY, val, count } = props.data;
+	let thisStyle = {
+		left: pgX - 150 > 0 ? `${pgX - 75}px` : `${0}px`,
+		top: `${pgY - 150}px`,
+		display: 'inline-block',
+		color: 'white',
+		backgroundColor: `rgba(0,0,0,.7)`
+	}
+
+	console.log('- - - - -')
 	return(
-		<div className='toolTip' style={{display:'block'}}>
+		<div className='toolTip' style={thisStyle}>
 			<p>Dummy Recat Component here</p>
 		</div>
 	);

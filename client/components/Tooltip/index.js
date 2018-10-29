08154bdabdd1de22abcaef1d60d3af3d98736c60
Eric Laursen <ericjlaursen@gmail.com>
2018-10-29 05:47:05 -0400

    Includes option for manual value-text prop.

--- client/components/Tooltip/index.js
+++ client/components/Tooltip/index.js
@@ -4,7 +4,7 @@ import './index.css';
 export default function Tooltip(props) {
 	console.log('tooltip props')
 	console.log(props.data)
-	let { pgX, pgY, val, count } = props.data;
+	let { pgX, pgY, val, valTxt, count } = props.data;
 	let thisStyle = {
 		left: pgX - 150 > 0 ? `${pgX - 75}px` : `${0}px`,
 		top: `${pgY - 150}px`,
@@ -17,7 +17,7 @@ export default function Tooltip(props) {
 	console.log('- - - - -')
 	return(
 		<div className='toolTip' style={thisStyle}>
-			<p><b>{val}</b> happens</p>
+			<p><b>{val}</b>{ valTxt || ' happens'}</p>
 			<p><b>{count}</b> times</p>
 		</div>
 	);

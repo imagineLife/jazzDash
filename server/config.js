5214208c2e3b8d870f716e45cfdd5062d965e121
Eric Laursen <ericjlaursen@gmail.com>
2018-11-30 07:06:26 -0500

    Uses mongo cloud url

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost:27017/jazzDashboard';
-                      // 'mongodb://mretfaster86:Jazz8Dash6@jazzdashdb-shard-00-00-65e8w.mongodb.net:27017,jazzdashdb-shard-00-01-65e8w.mongodb.net:27017,jazzdashdb-shard-00-02-65e8w.mongodb.net:27017/jazzDashboard?ssl=true&replicaSet=JazzDashDB-shard-0&authSource=admin&retryWrites=true'
+                      // 'mongodb://localhost:27017/jazzDashboard';
+                      'mongodb://mretfaster86:Jazz8Dash6@jazzdashdb-shard-00-00-65e8w.mongodb.net:27017,jazzdashdb-shard-00-01-65e8w.mongodb.net:27017,jazzdashdb-shard-00-02-65e8w.mongodb.net:27017/jazzDashboard?ssl=true&replicaSet=JazzDashDB-shard-0&authSource=admin&retryWrites=true'
 exports.TEST_DATABASE_URL = 'mongodb://localhost:27017/jazzDashboard-test';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;

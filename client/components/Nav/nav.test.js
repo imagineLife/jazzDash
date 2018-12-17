8bb5262eaa4a39670e154de187d7c1937d64ca60
Eric Laursen <ericjlaursen@gmail.com>
2018-12-17 11:58:28 -0500

    includes 2 more tests

--- client/components/Nav/nav.test.js
+++ client/components/Nav/nav.test.js
@@ -4,8 +4,23 @@ import Nav from './index';
 import { shallow } from 'enzyme';
 
 describe('Nav', () => {
+
+	//smoke tests
 	it('shallow renders without crashing', () => {
 	  shallow(<Nav />);
 	});	
+
+	it('has correct class', () => {
+        const nav = shallow(<Nav />);
+        expect(nav.hasClass('nav')).toEqual(true);
+    });
+
+    it('has hard-coded children count of 2', () => {
+    	const nav = shallow(<Nav />);
+		expect(nav.find('ul').children().length).toEqual(2);    	
+    })
+
+  
+	//how to test rendering children ie. nav with <NavLink />
 })
 

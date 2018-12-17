7155de2de17440643cae6561acb78dcf0da770d3
Eric Laursen <ericjlaursen@gmail.com>
2018-12-17 15:15:15 -0500

    tests link urls, and validates NavLinks render

--- client/components/Nav/nav.test.js
+++ client/components/Nav/nav.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Nav from './index';
-import { shallow } from 'enzyme';
+import NavLink from '../NavLink/index';
+import { shallow, mount } from 'enzyme';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 describe('Nav', () => {
 
@@ -20,7 +22,28 @@ describe('Nav', () => {
 		expect(nav.find('ul').children().length).toEqual(2);    	
     })
 
-  
-	//how to test rendering children ie. nav with <NavLink />
+    it('renders two NavLinks child components', () => {
+    	const nav = mount(<Router><Nav /></Router>);
+		expect(nav.find('ul').childAt(0).type()).toEqual(NavLink)
+		expect(nav.find('ul').childAt(1).type()).toEqual(NavLink)
+    })
+
+    it('links first to "/"', () => {
+	    const mockCallBack = jest.fn();
+	    const nav = mount(<Router><Nav /></Router>);
+	    const button = nav.find(NavLink);
+	    expect(nav.find(NavLink).first().props().linkTo).toBe('/');
+	});
+
+	it('links second to "/about"', () => {
+	    const mockCallBack = jest.fn();
+	    const nav = mount(<Router><Nav /></Router>);
+	    const button = nav.find(NavLink);
+	    // console.log(button.debug());
+	    expect(nav.find(NavLink).at(1).props().linkTo).toBe('/about');
+	    // button.simulate('click');
+	    // expect(mockCallBack.mock.calls.length).toEqual(1);
+	});
+
 })
 

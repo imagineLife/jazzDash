c4dbe6c9ac012e9df2ad8802e1de3682b941a71f
Eric Laursen <ericjlaursen@gmail.com>
2019-01-06 15:15:40 -0500

    2 new tests

--- client/containers/CountByNoteName/cbnn.test.js
+++ client/containers/CountByNoteName/cbnn.test.js
@@ -1,34 +1,52 @@
 import React from 'react';
-import cbnn from '../../../__mocks__/dummyBarData.js';
+import mockComponentData from '../../../__mocks__/dummyBarData.js';
 import {CountByNoteName} from './index'
+import Rect from '../../components/Rect'
 import ResponsiveWrapper from '../ResponsiveWrapper';
-import { shallow, mount } from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
+
+//jest.mock replacement
+mockComponentData.showTooltip = () => {};
+mockComponentData.hideTooltip =  () => {};
 
 describe('CHART CountByNoteName', () =>{
+
+	// let cbnnMount = null;
+	//before 
+	// beforeAll(() => {
+	// 	cbnnMount = mount(<CountByNoteName {...mockComponentData}/>);
+	// })
+
+	//let cbnn = mount(<CountByNoteName {...mockComponentData}/>);
+
 	it('RENDERS w dummy props & mock data without crashing', () => {
-		let dummyProps = {
-			showTooltip: jest.mock(),
-			hideTooltip: jest.mock(),
-			data: cbnn,
-			parentCol: '12',
-			respWrapWidth: 502
-		}
-		shallow(<CountByNoteName {...dummyProps}/>);
+		shallow(<CountByNoteName {...mockComponentData}/>);
 	})
 
-	it.only('displays correct # of bars from props', () => {
-		let dummyProps = {
-			showTooltip: jest.mock(),
-			hideTooltip: jest.mock(),
-			data: cbnn,
-			parentCol: '12',
-			respWrapWidth: 502
-		}
-		let parent = shallow(<CountByNoteName {...dummyProps}/>);
-		console.log('parent')
-		// console.log(parent.find('.singleBar').length) not correct right not
-		console.log(parent.debug())
+	it('displays correct # of bars from props', () => {
+		let cbnn = render(<CountByNoteName {...mockComponentData}/>);
+		expect(cbnn.find('Rect').length).toEqual(12) 
+	})
+
+	it.only('toggles a bar as expected', () => {
 		
-		// expect(parent.find(Rect).kength).to.equal(12)
+		//1. render the chart component
+		let cbnnMount = mount(<CountByNoteName {...mockComponentData}/>);
+		
+		//2. get a bar val
+		let firstBarProps = cbnnMount.find('Rect').first().props().count
+		// expect(firstBarProps).toEqual(mockComponentData.data[0]["A"])
+
+		//3.Find & toggle the toggle
+		let toggle = cbnnMount.find('[type="checkbox"]').simulate('click')
+
+		//4. get same bar val as before
+		let barValAfterClick = cbnnMount.find('Rect').first().props().count
+		
+		//expect BEFORE & AFTER clicking toggle
+		expect(barValAfterClick).toEqual(mockComponentData.data[1]["A"])
+
+
+		expect(firstBarProps).toEqual(mockComponentData.data[0]["A"])
 	})
 })

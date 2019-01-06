d70e2e383357de3d360928a4fd17dff2369645b8
Eric Laursen <ericjlaursen@gmail.com>
2019-01-06 17:56:48 -0500

    updates code layout

--- client/components/Toggle/toggle.test.js
+++ client/components/Toggle/toggle.test.js
@@ -23,51 +23,50 @@ describe('Tests Toggle', () => {
 	  shallow(<Toggle {...dummyProps}/>);
 	});
 
-    it('gets class from props', () => {
-        const toggle = mount(<Toggle {...dummyProps}/>);
-        const togLabel = toggle.childAt(0)
-        expect(togLabel.hasClass(dummyProps.cl)).toEqual(true);
-    })
+  it('gets class from props', () => {
+      const toggle = mount(<Toggle {...dummyProps}/>);
+      const togLabel = toggle.childAt(0)
+      expect(togLabel.hasClass(dummyProps.cl)).toEqual(true);
+  })
 
-    it('sets class in input', () => {
-      const toggle = render(<Toggle {...dummyProps}/>); 
-      let inputElement = toggle.find(`.toggleMusician${dummyProps.cl}`).first();
-      expect(inputElement).toBeTruthy();
-    })
+  it('sets class in input', () => {
+    const toggle = render(<Toggle {...dummyProps}/>); 
+    let inputElement = toggle.find(`.toggleMusician${dummyProps.cl}`).first();
+    expect(inputElement).toBeTruthy();
+  })
 
-    it('sets ID in input', () => {
-      const toggle = render(<Toggle {...dummyProps}/>); 
-      let inputElement = toggle.find(`toggleMusician${dummyProps.cl}`).first();
-      expect(inputElement).toBeTruthy();
-    })
+  it('sets ID in input', () => {
+    const toggle = render(<Toggle {...dummyProps}/>); 
+    let inputElement = toggle.find(`toggleMusician${dummyProps.cl}`).first();
+    expect(inputElement).toBeTruthy();
+  })
 
-    it('sets name in input', () => {
-      const toggle = render(<Toggle {...dummyProps}/>); 
+  it('sets name in input', () => {
+    const toggle = render(<Toggle {...dummyProps}/>); 
 
-      let inputElement = toggle.find(`[name="toggleMusician${dummyProps.cl}"]`).first();
-      expect(inputElement).toBeTruthy();
-    })
+    let inputElement = toggle.find(`[name="toggleMusician${dummyProps.cl}"]`).first();
+    expect(inputElement).toBeTruthy();
+  })
 
-    //why doesnt this work either!?
-    it('sets span texts from props', () => {
-      const toggle = mount(<Toggle {...dummyProps} />);
-      let sliderDiv = toggle.find('.slider')
-      expect(sliderDiv.find('span').first().text()).toEqual(dummyProps.opts.second)
-    })
+  //why doesnt this work either!?
+  it('sets span texts from props', () => {
+    const toggle = mount(<Toggle {...dummyProps} />);
+    let sliderDiv = toggle.find('.slider')
+    expect(sliderDiv.find('span').first().text()).toEqual(dummyProps.opts.second)
+  })
 
-    it('sets both span class from props', () => {
-        const toggle = shallow(<Toggle {...dummyProps} />);
-        console.log(toggle.debug());
-        expect(toggle.find(`.${dummyProps.cl}`).length).toEqual(2)
-    })
+  it('sets both span class from props', () => {
+      const toggle = shallow(<Toggle {...dummyProps} />);
+      expect(toggle.find(`.${dummyProps.cl}`).length).toEqual(2)
+  })
 
-    //help test CLICKING / changing the toggle
-    it.only('changes which span is highlighted onCheckboxToggle', () => {
-        const toggle = shallow(<Toggle {...dummyProps} />);
-        let rightSpan = toggle.find('.rightSpan')
+  //help test CLICKING / changing the toggle
+  it('changes which span is highlighted onCheckboxToggle', () => {
+      const toggle = shallow(<Toggle {...dummyProps} />);
+      let rightSpan = toggle.find('.rightSpan')
 
-        //seems to have only ONE span in debug output ...?
-        console.log(slider.debug())
-    })
+      //seems to have only ONE span in debug output ...?
+      // console.log(slider.debug())
+  })
 })
 

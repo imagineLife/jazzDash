2fc517f3fc2cf5e384edafca42de586f6d2b0615
Eric Laursen <ericjlaursen@gmail.com>
2019-01-08 06:42:12 -0500

    more mock data

--- __mocks__/dummyBarData.js
+++ __mocks__/dummyBarData.js
@@ -1,59 +1,50 @@
-const cbnn = [
-	{
-		A: 29,
-		Ab: 17,
-		B: 7,
-		Bb: 44,
-		C: 47,
-		D: 39,
-		Db: 8,
-		E: 13,
-		Eb: 43,
-		F: 91,
-		G: 38,
-		Gb: 9,
-		grWidth: "12",
-		_musician: {
-			first: "Joe",
-			last: "Henderson",
-			_id: "5b57ae2a15565c1859808e35"
-		},
-		_song:{
-			album: "Unity",
-			title: "If",
-			_id: "5b59a3a4e8aaa194e036155a"
-		},
-		album: "Unity",
-		title: "If",
-		_id: "5b59a3a4e8aaa194e036155a"
-	},
-	{
-		A: 28,
-		Ab: 41,
-		B: 13,
-		Bb: 79,
-		C: 34,
-		D: 23,
-		Db: 17,
-		E: 17,
-		Eb: 45,
-		F: 55,
-		G: 11,
-		Gb: 14,
-		grWidth: "12",
-		_musician: {
-			first: "Joe",
-			last: "Henderson",
-			_id: "5b57ae2a15565c1859808e35"
-		},
-		_song:{
-			album: "Unity",
-			title: "If",
-			_id: "5b59a3a4e8aaa194e036155a"
-		},
-		album: "Unity",
-		title: "If",
-		_id: "5b59a3a4e8aaa194e036155a"
-	}
-]
-module.exports = cbnn;
+const mockData = {
+  "data": [
+    {
+      "musician": "Joe Henderson",
+      "song": {
+        "_id": "5b59a3a4e8aaa194e036155a",
+        "title": "If",
+        "album": "Unity"
+      },
+      "grWidth": "12",
+      "A": 29,
+      "Bb": 44,
+      "B": 7,
+      "C": 47,
+      "Db": 8,
+      "D": 39,
+      "Eb": 43,
+      "E": 13,
+      "F": 91,
+      "Gb": 9,
+      "G": 38,
+      "Ab": 17
+    },
+    {
+      "musician": "Woody Shaw",
+      "song": {
+        "_id": "5b59a3a4e8aaa194e036155a",
+        "title": "If",
+        "album": "Unity"
+      },
+      "grWidth": "12",
+      "A": 28,
+      "Bb": 79,
+      "B": 13,
+      "C": 34,
+      "Db": 17,
+      "D": 23,
+      "Eb": 45,
+      "E": 17,
+      "F": 55,
+      "Gb": 14,
+      "G": 11,
+      "Ab": 41
+    }
+  ],
+  "parentCol": "12",
+  "respWrapWidth": 343.6875
+}
+
+module.exports = mockData;

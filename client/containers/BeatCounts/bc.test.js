2cc022b1e914759bdfc1c4006a9c7b76a59bed19
Eric Laursen <ericjlaursen@gmail.com>
2019-01-19 18:36:46 -0500

    Includes a toggle-checking test

--- client/containers/BeatCounts/bc.test.js
+++ client/containers/BeatCounts/bc.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import beatNumberFreqs from '../../../__mocks__/beatNumberFreqs.js';
 import {BeatCounts} from './index'
-import Rect from '../../components/Rect'
+import SingleText from '../../components/SingleText'
 import ResponsiveWrapper from '../ResponsiveWrapper';
 import { shallow, mount, render } from 'enzyme';
 
@@ -11,26 +11,19 @@ beatNumberFreqs.hideTooltip =  () => {};
 
 describe('CHART BeatCounts', () =>{
 
-	// let cbnnMount = null;
-	//before 
-	// beforeAll(() => {
-	// 	cbnnMount = mount(<BeatCounts {...beatNumberFreqs}/>);
-	// })
-
-	//let cbnn = mount(<BeatCounts {...beatNumberFreqs}/>);
-
 	it('RENDERS w dummy props & mock data without crashing', () => {
 		shallow(<BeatCounts {...beatNumberFreqs}/>);
 	})
 
-	//check how many COMPONENTS are rendered next
-	//not just how many svg elements there are
+
+	//I'd like to figure out how to check how many 
+	//	react child COMPONENTS are rendered next ?!?!
 
 
-	it('displays correct # of SingleText elements from props', () => {
+	it('displays correct # of <text> elements from chart props', () => {
 
 		//render the Chart Component
-		let cbnn = render(<BeatCounts {...beatNumberFreqs}/>);
+		let beatCount = render(<BeatCounts {...beatNumberFreqs}/>);
 
 		//get number of expected bars from mock data
 		let firstDataSet = beatNumberFreqs.data[0];
@@ -40,27 +33,28 @@ describe('CHART BeatCounts', () =>{
 				return firstDataSet[d]
 			}else{ return null }
 		}).filter(d => d)
-
+		
 		//TEST
-		expect(cbnn.find('Text').length).toEqual(justRelevantBeats.length) 
+		expect(beatCount.find('Text').length).toEqual(justRelevantBeats.length) 
 	})
 
-	// it('correctly updates a bar value onToggle', () => {
+	it('correctly updates the first text value onToggle', () => {
 		
-	// 	//1. render the chart component
-	// 	let cbnnMount = mount(<BeatCounts {...beatNumberFreqs}/>);
+		//1. render the chart component
+		let beatCountMount = mount(<BeatCounts {...beatNumberFreqs}/>);
 		
-	// 	//2. get a bar val
-	// 	let firstBarProps = cbnnMount.find('Rect').first().props().count
+		//2. get a bar val
+		let firstTextVal = beatCountMount.find('text').first().props()['data-count'];
+		
+		//3.Find & toggle the toggle
+		let toggle = beatCountMount.find('[type="checkbox"]').simulate('click')
 
-	// 	//3.Find & toggle the toggle
-	// 	let toggle = cbnnMount.find('[type="checkbox"]').simulate('click')
-
-	// 	//4. get same bar val as before
-	// 	let barValAfterClick = cbnnMount.find('Rect').first().props().count
+		//4. get same bar val as before
+		let barValAfterClick = beatCountMount.find('text').first().props()['data-count'];
+		let matchingMockDataElement = beatNumberFreqs.data[1]
 		
-	// 	//expect BEFORE & AFTER clicking toggle barVals
-	// 	expect(firstBarProps).toEqual(beatNumberFreqs.data[0]["A"])
-	// 	expect(barValAfterClick).toEqual(beatNumberFreqs.data[1]["A"])
-	// })
+		//expect BEFORE & AFTER clicking toggle barVals
+		expect(firstTextVal).toEqual(beatNumberFreqs.data[0][0].count)
+		expect(barValAfterClick).toEqual(beatNumberFreqs.data[1][0].count)
+	})
 })

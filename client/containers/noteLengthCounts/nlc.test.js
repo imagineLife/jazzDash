6a86e552ce64ab5fdc6fbbbfcf07df2f8f6990b9
Eric Laursen <ericjlaursen@gmail.com>
2019-01-19 13:00:38 -0500

    comments last test, renders 0 bars due to animation

--- client/containers/noteLengthCounts/nlc.test.js
+++ client/containers/noteLengthCounts/nlc.test.js
@@ -1,56 +1,61 @@
 import React from 'react';
-import mockComponentData from '../../../__mocks__/NoteLengthCounts.js';
+import mockNoteLengthData from '../../../__mocks__/NoteLengthCounts.js';
 import {NoteLengthCounts} from './index'
 import Rect from '../../components/Rect'
 import { shallow, mount, render } from 'enzyme';
 
 //jest.mock replacement
-mockComponentData.showTooltip = () => {};
-mockComponentData.hideTooltip =  () => {};
+mockNoteLengthData.showTooltip = () => {};
+mockNoteLengthData.hideTooltip =  () => {};
 
 describe('CHART NoteLengthCounts', () =>{
 
 	// let cbnnMount = null;
 	//before 
 	// beforeAll(() => {
-	// 	cbnnMount = mount(<NoteLengthCounts {...mockComponentData}/>);
+	// 	cbnnMount = mount(<NoteLengthCounts {...mockNoteLengthData}/>);
 	// })
 
-	//let cbnn = mount(<NoteLengthCounts {...mockComponentData}/>);
+	//let cbnn = mount(<NoteLengthCounts {...mockNoteLengthData}/>);
 
 	it('RENDERS w dummy props & mock data without crashing', () => {
-		shallow(<NoteLengthCounts {...mockComponentData}/>);
+		shallow(<NoteLengthCounts {...mockNoteLengthData}/>);
 	})
 
 	it('displays correct # of bars from props', () => {
 
 		//render the component with mock data as props
-		let cbnn = render(<NoteLengthCounts {...mockComponentData}/>);
+		let noteLengthCounts = render(<NoteLengthCounts {...mockNoteLengthData}/>);
 
 		//get number of expected bars from mock data
-		let mockBarsKeys = Object.keys(mockComponentData.data[0]).filter(d => !['musician','song','grWidth'].includes(d))
+		let mockBarsKeys = Object.keys(mockNoteLengthData.data[0]).filter(d => !['musician','song','grWidth'].includes(d))
 		let countOfInitialMockBars = mockBarsKeys.length
 
 		//TEST
-		expect(cbnn.find('Rect').length).toEqual(countOfInitialMockBars) 
+		expect(noteLengthCounts.find('Rect').length).toEqual(countOfInitialMockBars) 
 	})
 
-	// it('correctly updates a bar value onToggle', () => {
+	//HOW TEST RENDERING WITH ANIMATION -> CDM in chart
+	// it.only('correctly updates first bar value onToggle', () => {
 		
-	// 	//1. render the chart component
-	// 	let cbnnMount = mount(<NoteLengthCounts {...mockComponentData}/>);
+		//1. render the chart component
+		// let nlcMount = mount(<NoteLengthCounts {...mockNoteLengthData}/>);
 		
-	// 	//2. get a bar val
-	// 	let firstBarProps = cbnnMount.find('Rect').first().props().count
+		//2. get a bar val
+		// console.log('nlcMount')
+		// console.log(nlcMount.find('Rect').first())
+		
+		// let firstBarProps = nlcMount.find('Rect').first()//.props().count;
 
-	// 	//3.Find & toggle the toggle
-	// 	let toggle = cbnnMount.find('[type="checkbox"]').simulate('click')
+		//3.Find & toggle the toggle
+		// let toggle = nlcMount.find('[type="checkbox"]').simulate('click')
 
-	// 	//4. get same bar val as before
-	// 	let barValAfterClick = cbnnMount.find('Rect').first().props().count
+		//4. get same bar val as before
+		// let barValAfterClick = nlcMount.find('Rect').first().props().count
 		
-	// 	//expect BEFORE & AFTER clicking toggle barVals
-	// 	expect(firstBarProps).toEqual(mockComponentData.data[0]["A"])
-	// 	expect(barValAfterClick).toEqual(mockComponentData.data[1]["A"])
-	// })
+		// expect(nlcMount.find('Rect').first())
+		//expect BEFORE & AFTER clicking toggle barVals
+		// expect(firstBarProps).toEqual(mockNoteLengthData.data[0]["A"])
+		// expect(barValAfterClick).toEqual(mockNoteLengthData.data[1]["A"])
+	})
 })

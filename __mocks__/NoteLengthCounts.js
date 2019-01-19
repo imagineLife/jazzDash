297394690e592575bcc9b802b486f6ed9a4198f3
Eric Laursen <ericjlaursen@gmail.com>
2019-01-19 12:40:27 -0500

    mock data for chart

---
+++ __mocks__/NoteLengthCounts.js
@@ -0,0 +1,131 @@
+const mockData = {
+  "data": [
+    {
+      "0": {
+        "_id": "5bb2bd3d944ae2c1a5f319d7",
+        "count": 30,
+        "length": 0.16
+      },
+      "1": {
+        "_id": "5bb2bd3d944ae2c1a5f319d8",
+        "count": 224,
+        "length": 0.5
+      },
+      "2": {
+        "_id": "5bb2bd3d944ae2c1a5f319d9",
+        "count": 15,
+        "length": 0.33
+      },
+      "3": {
+        "_id": "5bb2bd3d944ae2c1a5f319da",
+        "count": 59,
+        "length": 1
+      },
+      "4": {
+        "_id": "5bb2bd3d944ae2c1a5f319db",
+        "count": 26,
+        "length": 1.5
+      },
+      "5": {
+        "_id": "5bb2bd3d944ae2c1a5f319dc",
+        "count": 3,
+        "length": 2
+      },
+      "6": {
+        "_id": "5bb2bd3d944ae2c1a5f319dd",
+        "count": 3,
+        "length": 2.5
+      },
+      "7": {
+        "_id": "5bb2bd3d944ae2c1a5f319de",
+        "count": 24,
+        "length": 0.25
+      },
+      "8": {
+        "_id": "5bb2bd3d944ae2c1a5f319df",
+        "count": 1,
+        "length": 3
+      },
+      "musician": "Joe Henderson",
+      "song": {
+        "_id": "5b59a3a4e8aaa194e036155a",
+        "title": "If",
+        "album": "Unity"
+      },
+      "grWidth": "6"
+    },
+    {
+      "0": {
+        "_id": "5bb2bd3d944ae2c1a5f319e0",
+        "count": 1,
+        "length": 2.5
+      },
+      "1": {
+        "_id": "5bb2bd3d944ae2c1a5f319cc",
+        "count": 26,
+        "length": 0.33
+      },
+      "2": {
+        "_id": "5bb2bd3d944ae2c1a5f319cd",
+        "count": 2,
+        "length": 0.1
+      },
+      "3": {
+        "_id": "5bb2bd3d944ae2c1a5f319ce",
+        "count": 14,
+        "length": 0.25
+      },
+      "4": {
+        "_id": "5bb2bd3d944ae2c1a5f319cf",
+        "count": 3,
+        "length": 0.2
+      },
+      "5": {
+        "_id": "5bb2bd3d944ae2c1a5f319d0",
+        "count": 270,
+        "length": 0.5
+      },
+      "6": {
+        "_id": "5bb2bd3d944ae2c1a5f319d1",
+        "count": 1,
+        "length": 0.7
+      },
+      "7": {
+        "_id": "5bb2bd3d944ae2c1a5f319d2",
+        "count": 45,
+        "length": 1
+      },
+      "8": {
+        "_id": "5bb2bd3d944ae2c1a5f319d3",
+        "count": 1,
+        "length": 0.83
+      },
+      "9": {
+        "_id": "5bb2bd3d944ae2c1a5f319d4",
+        "count": 9,
+        "length": 1.5
+      },
+      "10": {
+        "_id": "5bb2bd3d944ae2c1a5f319d5",
+        "count": 1,
+        "length": 4.5
+      },
+      "11": {
+        "_id": "5bb2bd3d944ae2c1a5f319d6",
+        "count": 4,
+        "length": 2
+      },
+      "musician": "Woody Shaw",
+      "song": {
+        "_id": "5b59a3a4e8aaa194e036155a",
+        "title": "If",
+        "album": "Unity"
+      },
+      "grWidth": "6"
+    }
+  ],
+  "parentCol": "6",
+  "respWrapWidth": 363.84375
+}
+
+module.exports = mockData;
